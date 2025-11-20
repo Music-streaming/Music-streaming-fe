13d1bc26a7e8887960b959243619cc86552f0796
yeonow <florau@knu.ac.kr>
2025-11-20 13:17:50 +0900

    fix/dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './login';
+import Login from './pages/login';
 import Home from './pages/Home';
 import MyPage from './pages/MyPage';
 import SignUp from './pages/SignUp';
